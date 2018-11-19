96b8f395b98e0c03a2e7c20410c346380a3651f6
Sifflex <sifflexsensei@gmail.com>
2018-11-19 20:37:15 +0100

    Adding source code

---
+++ Bertrand Renard/Calculations.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bertrand_Renard
+{
+    public static class Calculations
+    {
+        private static List<double> List_Without_Digit(List<double> list, double i) // Returns a list without i
+        {
+            List<double> without_i = new List<double>();
+            int without_i_length = list.Count;
+            int j = 0;
+            while (j < without_i_length && list[j] != i)
+            {
+                without_i.Add(list[j]);
+                j++;
+            }
+            j++;
+            while (j < without_i_length)
+            {
+                without_i.Add(list[j]);
+                j++;
+            }
+            return without_i;
+        }
+
+        private static void Find_Combi(List<double> digits, double nb, double current, string result, ref List<string> output_list) // Fill output_list with all solutions
+        {
+            // Percentage(calls, max_calls);
+            if (digits.Count != 0)
+            {
+                string combi = "";
+                string res;
+                foreach (double i in digits)
+                {
+                    List<double> otherdigits = List_Without_Digit(digits, i);
+                    if (current + i == nb)
+                    {
+                        res = result + " + " + i.ToString();
+                        output_list.Add(res);
+                    }
+                    if (current - i == nb)
+                    {
+                        res = result + " - " + i.ToString();
+                        output_list.Add(res);
+                    }
+                    if (current * i == nb)
+                    {
+                        res = result + " * " + i.ToString();
+                        output_list.Add(res);
+                    }
+                    if (i != 0 && current / i == Math.Truncate(current / i) && current / i == nb)
+                    {
+                        res = result + " / " + i.ToString();
+                        output_list.Add(res);
+                    }
+                    if (otherdigits.Count != 0)
+                    {
+                        Find_Combi(otherdigits, nb, current + i, "(" + result + ") + " + i.ToString(), ref output_list);
+                        Find_Combi(otherdigits, nb, current - i, "(" + result + ") - " + i.ToString(), ref output_list);
+                        Find_Combi(otherdigits, nb, current * i, "(" + result + ") * " + i.ToString(), ref output_list);
+                        Find_Combi(otherdigits, nb, current + i, "(" + result + ") / " + i.ToString(), ref output_list);
+                        Find_Combi(otherdigits, nb - current, 0, result + ")", ref output_list);
+                        Find_Combi(otherdigits, nb - current, 0, result + ")", ref output_list);
+                        Find_Combi(otherdigits, nb - current, 0, result + ")", ref output_list);
+                        Find_Combi(otherdigits, nb - current, 0, result + ")", ref output_list);
+                    }
+                }
+            }
+        }
+
+        public static string Solution(double tofind, double start, List<double> alldigits) // Get solution
+        {
+            List<string> result = new List<string>();
+            // List<int> maxs = new List<int> { 1, 17, 409, 13089, 523561, 25130929 };
+            int cou = alldigits.Count;
+            Find_Combi(alldigits, tofind, start, "", ref result); // Fill result with solutions
+            if (result.Count != 0)
+            {
+                int j = 0;
+                int pos = 0;
+                int min = result[0].Length;
+                while (j < result.Count) // Determine shortest solution
+                {
+                    string curr = result[j];
+                    if (curr.Length < min)
+                    {
+                        pos = j;
+                        min = curr.Length;
+                    }
+                    j++;
+                }
+                string solution = result[pos];
+                return Format_Solution(solution);
+            }
+            return "Il n'y a pas de solution.";
+        }
+
+        private static string Format_Solution(string solution) // Get solution to be more readable
+        {
+            bool changed = true;
+            string solution_changed;
+            while (changed)
+            {
+                changed = false;
+                solution_changed = solution.Replace("()", "");
+                if (solution != solution_changed)
+                {
+                    solution = solution_changed;
+                    changed = true;
+                }
+                int open_par = 0;
+                int close_par = 0;
+                int z = 0;
+                while (z < solution.Length)
+                {
+                    if (solution[z] == '(')
+                        open_par++;
+                    if (solution[z] == ')')
+                        close_par++;
+                    z++;
+                }
+                for (int m = 0; m < open_par - close_par; m++) // Remove excessive open parenthesis
+                {
+                    changed = true;
+                    if (solution[0] == '(')
+                    {
+                        solution = solution.Substring(1);
+                    }
+                }
+            }
+            return solution;
+        }
+
+        private static void Percentage(double current, double max) // Displays % but multiplies time by 120
+        {
+            Console.CursorLeft = 0;
+            Console.Write(Math.Round((current/max * 100)).ToString() + "%");
+        }
+    }
+}
---
+++ Bertrand Renard/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Bertrand_Renard
+{
+    class Program
+    {
+        public static double to_find;
+        static List<double> digits = new List<double>();
+        static double a; // For ab^x
+        static double b;
+        static void Main(string[] args)
+        {
+            Predict_Time();
+            Stopwatch sw = new Stopwatch();
+            bool exit = false;
+            bool keep_list = false;
+
+            while (!exit)
+            {
+                string solution;
+
+                to_find = 0;
+                Get_to_find(); // Get objective
+
+                if (!keep_list)
+                {
+                    digits = new List<double>();
+                    Get_Digits();  // Fill digits list
+                }
+
+                double time = a * Math.Pow(b, digits.Count) / 1000;
+                if (time > 1)
+                    Console.WriteLine("Temps nécessaire : moins de {0} secondes", Math.Truncate(time).ToString());
+                else
+                    Console.WriteLine("Temps nécessaire : environ {0} millisecondes", (time * 1000).ToString());
+                sw.Restart();
+
+                solution = Calculations.Solution(to_find, 0, digits);
+                Console.WriteLine();
+                Console.WriteLine(solution + " -> temps : " + sw.ElapsedMilliseconds.ToString() + "ms");
+                Console.WriteLine();
+
+                Console.WriteLine("Pour fermer appyer sur q, pour recommencer appuyer sur une autre touche, appuyer sur k pour conserver la liste");
+                string exit_string = Console.ReadKey().Key.ToString();
+                if (exit_string != "Q")
+                {
+                    keep_list = false;
+                    if (exit_string == "K")
+                    {
+                        keep_list = true;
+                    }
+                    Console.Clear();
+                }
+                else
+                {
+                    exit = true;
+                }
+            }
+        }
+
+        static void Get_to_find()
+        {
+            bool has_chosen = false;
+            while (!has_chosen)
+            {
+                Console.WriteLine("Quel chiffre Bertrand-Renardiser ?");
+                try
+                {
+                    to_find = double.Parse(Console.ReadLine());
+                    has_chosen = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Mets un bon chiffre ??");
+                }
+            }
+        }
+
+        static void Get_Digits()
+        {
+            string digit = "";
+            while (true)
+            {
+                Console.WriteLine("Quel chiffre ajouter à la liste pour Bertrand-Renardiser (supporte le format x, y, z ...) ? mets y si c'est fini");
+                Console.Write("Chiffres actuels : ");
+                int cou = digits.Count;
+                if (cou > 0)
+                {
+                    for (int i = 0; i < cou - 1; i++)
+                    {
+                        Console.Write(digits[i].ToString() + ", ");
+                    }
+                    Console.Write(digits[cou - 1].ToString());
+                    Console.WriteLine();
+                }
+                try
+                {
+                    digit = Console.ReadLine();
+                    if (digit.ToLower() == "y")
+                    {
+                        if (digits.Count > 0)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Il n'y a aucun chiffre dans la liste.");
+                        }
+                    }
+                    else if (digit.Contains(","))
+                    {
+                        int j = 0;
+                        while (j < digit.Length) // Add format being like 3, 7, 9, 2...
+                        {
+                            string current_digit = "";
+                            while (j < digit.Length && digit[j] >= 48 && digit[j] <= 57)
+                            {
+                                current_digit += digit[j];
+                                j++;
+                            }
+                            if (current_digit.Length > 0)
+                            {
+                                digits.Add(double.Parse(current_digit));
+                            }
+                            j++;
+                        }
+                    }
+                    else
+                    {
+                        digits.Add(double.Parse(digit));
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Mets un bon chiffre ??");
+                }
+            }
+        }
+
+        static void Predict_Time()
+        {
+            Stopwatch st = new Stopwatch();
+            List<string> list_time_string = new List<string>();
+            List<double> list_time_float = new List<double>() { 1, 1, 1, 1 };
+            double time1;
+            double time2;
+
+            st.Start();
+            Calculations.Solution(1, 0, list_time_float);
+            time1 = st.ElapsedMilliseconds;
+
+            list_time_float.Add(1);
+
+            st.Restart();
+            Calculations.Solution(1, 0, list_time_float);
+            time2 = st.ElapsedMilliseconds;
+
+            b = time2 / time1;
+            a = time2 / Math.Pow(b, 5) * 0.3;
+        }
+    }
+}
